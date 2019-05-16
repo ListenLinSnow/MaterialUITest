15b72de192028f548f34e42a57158853bef02860
QingMi <2543249349@qq.com>
2019-05-16 11:31:22 +0800

    sixth commit

--- app/src/main/java/com/example/lc/materialuitest/activity/ProgressBarActivity.java
+++ app/src/main/java/com/example/lc/materialuitest/activity/ProgressBarActivity.java
@@ -6,9 +6,14 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.animation.LinearInterpolator;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.example.lc.materialuitest.R;
 
@@ -28,6 +33,10 @@ public class ProgressBarActivity extends AppCompatActivity {
     ProgressBar pbBuffer;
     @BindView(R.id.pb_progress_bar_determinate_and_indeterminate)
     ProgressBar pbMixing;
+    @BindView(R.id.sb_progress_bar)
+    SeekBar seekBar;
+    @BindView(R.id.et_progress_bar)
+    EditText editText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,7 +48,40 @@ public class ProgressBarActivity extends AppCompatActivity {
     }
 
     private void init(){
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                editText.setText(progress + "");
+                Toast.makeText(ProgressBarActivity.this, progress + "", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int value = Integer.valueOf(s.toString());
+                seekBar.setProgress(value);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
 }
